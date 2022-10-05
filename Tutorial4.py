5a2edb1fd98cbada601374ee0205b30fd198c630
Freelance.py <89837926+Hackersaran@users.noreply.github.com>
2022-10-05 09:26:50 +0530

    Python Tutorials

---
+++ Tutorial4.py
@@ -0,0 +1,35 @@
+# Python Variable
+
+## Creating Variables
+
+x = 'SARAN'             # x is a str
+y = 12062008            # y is a int
+print(x,y)
+
+## Casting
+
+x = str(3)              # x is '3'
+y = int(3)              # y is 3
+z = float(3)            # z is 3.0
+print(x,y,z)
+
+## Get the Type
+
+x = 12
+y = 'Saran'
+z = 4.125
+print(type(x))
+print(type(y))
+print(type(z))
+
+## Single or Double Quotes?
+
+x = "Full Stack Web Developer"  
+# is the same prints one time
+x = "Full Stack Web Developer"
+print(x)
+
+## Case-Sensitive
+
+x = "Full Stack Web Developer"           # X will Not Overwrite on x
+X = "Full Stack Web Developer"
