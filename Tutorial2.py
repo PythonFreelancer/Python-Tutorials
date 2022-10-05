d63e0a03908638022bdcce2bf98357ede791e468
Freelance.py <89837926+Hackersaran@users.noreply.github.com>
2022-10-05 09:25:19 +0530

    Python Tutorials

---
+++ Tutorial2.py
@@ -0,0 +1,27 @@
+# Python Syntax
+
+## Execute Python Syntax
+
+print('Hello World!')
+
+## Python Indentation
+
+if 5 > 2:
+  print("Full Stack Web Developer")
+
+'''if 5 > 2:
+print("Full Stack Web Developer")      !WRONG!''' 
+
+'''if 5>2:
+    print('Full Stack Web Developer')
+     print('Full Stack Web Developer') !WRONG!'''
+
+## Python Variables
+
+x = 2
+z = 'Full Stack Web Developer'
+
+## Comments
+
+# This is Comment
+print('Saran')
