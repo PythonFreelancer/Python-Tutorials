3e5bace9a2ba432449a32929836f1bf746f33e33
Freelance.py <89837926+Hackersaran@users.noreply.github.com>
2022-10-05 09:26:06 +0530

    Python Tutorials

---
+++ Tutorial3.py
@@ -0,0 +1,19 @@
+#Python Comment
+
+## Creating a Comment
+
+#This is a Comment
+print('Saran')
+
+## Multi Line Comments
+
+#trnydrdunrtundr
+#unrudr6udr6ut7u
+#r6ur6nur6nu6nu
+
+    #or
+
+'''dytrhuyhrunu
+un7u7u7ti7ti7
+nni6i68i68ii
+7ni7i7i7ni8iin'''
