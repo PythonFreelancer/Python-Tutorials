60d18e8668da822746ca2c8879d09ee8e5828527
Freelance.py <89837926+Hackersaran@users.noreply.github.com>
2022-10-09 21:05:51 +0530

    Python Tutorials

---
+++ Tutorial7.py
@@ -0,0 +1,14 @@
+# Python - Output Variables
+
+## Output Variables
+
+a = 'Freelancer'
+b = 'Python'
+c = 'Html'
+d = 12
+e = 6
+print(a,b,c)
+print(a+b+c)
+print(d+e+d+e)
+print(a,d)
+#print(a+d) !WRONG!
