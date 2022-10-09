672d50f29a9f1fa33e4065ab3e22af93f1e41c36
Freelance.py <89837926+Hackersaran@users.noreply.github.com>
2022-10-09 21:04:33 +0530

    Python Tutorials

---
+++ Tutorial6.py
@@ -0,0 +1,24 @@
+# Python Variables - Assign Multiple Values
+
+## Many Values to Multiple Variables
+
+s, a, r = "Full", "Stack", "Web Developer"
+print(s)
+print(a)
+print(r)
+
+## One Value to Multiple Variables
+
+x=y=z='Full Stack Web Developer'
+print(x)
+print(y)
+print(z)
+
+## Unpack a Collection
+
+Cars = ['Audi','Aston Martin','Mini Cooper','Benz']
+w,x,y,z=Cars
+print(w)
+print(x)
+print(y)
+print(z)
