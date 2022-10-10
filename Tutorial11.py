ea693ae5f0688aee6c06e8bf07cec16e931e7312
Freelance.py <89837926+Hackersaran@users.noreply.github.com>
2022-10-10 17:32:28 +0530

    Python Tutorials

---
+++ Tutorial11.py
@@ -0,0 +1,36 @@
+# Python Casting
+
+int()
+str()
+float()
+
+## Specify a Variable Type
+
+int()
+
+e = '2'
+f = 30.30300
+g = 2
+print(int(e))
+print(int(f))
+print(int(g))
+
+float()
+
+a ='12'
+b = 4
+c = '24.0'
+d = 3.4545
+print(float(a))
+print(float(b))
+print(float(c))
+print(float(d))
+
+str()
+
+a = 2                         #'2'
+b = 3.444                     #'3.444'
+c = 3j                        #'3j'
+print(str(a))
+print(str(b))
+print(str(c))
