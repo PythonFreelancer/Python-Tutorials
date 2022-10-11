b6971435d7c25ef19de28f822f9fc11e13ec7cf3
Freelance.py <89837926+PythonFreelancer@users.noreply.github.com>
2022-10-11 21:28:41 +0530

    Python Tutorials

---
+++ Tutorial12.py
@@ -0,0 +1,54 @@
+# Python Strings
+
+## Strings
+
+print('Hello , Saran')           # 'Hello , Saran' is a string
+
+## Assign String to a Variable
+
+g = 'Saran'
+print(g)
+
+## Multiline Strings
+
+# We Can Use Double Quote or Single Quote
+
+e =  '''ggfgccfrsrvvg                      
+ebeybeyeyemd6r86858m88m
+byurumid6umm6ud7um67du6
+eybeeeeeeetytyyyyyyyyyy'''
+print(e)
+
+## Strings are Arrays
+
+d = 'Full Satck Web Developer'
+print(d[12])
+
+## Looping Through a String
+
+a = 'Python'
+for i in a:
+    print(i)
+
+## String Length
+
+r = 'Developer'
+print(len(r))                           # len()
+    
+## Check String
+
+textfile = 'I am A Freelancer'  
+print('Freelancer' in textfile)         # in
+
+
+if 'Freelancer' in textfile:                          # if
+    print('Yes, in textfile Freelancer is there')
+    print('Freelancer')
+
+## Check if NOT
+
+textfile = 'I am the developer'
+print('Best'not in textfile )
+
+if 'Best' not in textfile:
+    print('yes, Best is not in textfile')
