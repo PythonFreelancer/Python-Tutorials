97f270f6cf39d57861ae5600df24afec3b9dc3fb
Galactic1239a <89837926+PythonFreelancer@users.noreply.github.com>
2022-10-17 07:29:47 +0530

    Python Tutorials

---
+++ Tutorial20.py
@@ -0,0 +1,83 @@
+# Python Operators
+
+## Python Operators
+
+print(200+63)
+
+'''
+
+Python divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+
+'''
+
+## Python Arithmetic Operators
+
+print(10+12)
+print(10-12)
+print(10/12)
+print(10*12)
+print(10%12)
+print(10**12)
+print(10//12)
+
+## Python Assignment Operators
+
+x = 5	         #x = 5	
+x += 3	         #x = x + 3	
+x -= 3	         #x = x - 3	
+x *= 3	         #x = x * 3	
+x /= 3	         #x = x / 3	
+x %= 3	         #x = x % 3	
+x //= 3	         #x = x // 3	
+x **= 3	         #x = x ** 3	
+x &= 3	         #x = x & 3	
+x |= 3	         #x = x | 3	
+x ^= 3	         #x = x ^ 3	
+x >>= 3	         #x = x >> 3	
+x <<= 3	         #x = x << 3
+
+## Python Comparison Operators
+
+x == x              
+x != x
+x > x	
+x < x	
+x >= x
+x <= x
+
+## Python Logical Operators
+
+x < 5 and  x < 10                            # and
+x < 5 or x < 4                               # or
+not(x < 5 and x < 10)                        # not
+
+## Python Identity Operators
+
+x is x                                       # is
+x is not x                                   # is not
+
+## Python Membership Operators
+
+x in x                                       # in
+x not in x                                   # not in
+
+## Python Bitwise Operators
+
+'''
+
+x & 12
+x | 12
+x^100
+x ~ x
+x<<12
+12>>x
+
+'''
