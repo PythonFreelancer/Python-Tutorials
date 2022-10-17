d1f66a269d23182ad424ad9c70a516c64ab333b6
Galactic1239a <89837926+PythonFreelancer@users.noreply.github.com>
2022-10-17 07:22:25 +0530

    Python Tutorials

---
+++ Tutorial14.py
@@ -0,0 +1,26 @@
+# Python - Modify Strings
+
+## Upper Case
+
+c = 'Developer'
+print(c.upper())
+
+## Lower Case
+
+r = 'DEVELOPER'
+print(r.lower())
+
+## Remove Whitespace
+
+s = 'Developer'
+print(s.strip())
+
+## Replace String
+
+t = 'Web Developer'
+print(t.replace('Web','Python'))
+
+## Split String
+
+a = 'Python Devloper'
+print(a.split())
