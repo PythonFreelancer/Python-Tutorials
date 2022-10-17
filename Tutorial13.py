9ae7813ef6640c135a87710bdb6d6e786e2a84b2
Galactic1239a <89837926+PythonFreelancer@users.noreply.github.com>
2022-10-17 07:19:29 +0530

    Python Tutorials

---
+++ Tutorial13.py
@@ -0,0 +1,22 @@
+# Python - Slicing Strings
+
+## Slicing
+
+t = 'Developer'
+print(t[0:5])
+
+## Slice From the Start
+
+j = 'Developer' 
+print(j[:6])
+
+## Slice To the End
+
+l = 'Developer'
+print(l[3:])
+
+## Negative Indexing
+
+s = 'Developer'
+print(s[-8:9])
+
