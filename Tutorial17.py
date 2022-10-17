eb213cffa4a9b9ce4eb2cecaf2c3f2366bbf35e5
Galactic1239a <89837926+PythonFreelancer@users.noreply.github.com>
2022-10-17 07:27:04 +0530

    Python Tutorials

---
+++ Tutorial17.py
@@ -0,0 +1,40 @@
+# Python - Escape Characters
+
+## Escape Character
+
+'''q = 'The best programming language is 'Python'''
+q = 'The best programming language is "Python"'
+a = 'The best programming language is \'Python\''
+print(q)
+print(a)
+
+## Escape Characters
+
+
+
+# \'	    Single Quote	
+# \\	    Backslash	
+# \n	    New Line	
+# \r	    Carriage Return	
+# \t	    Tab	
+# \b	    Backspace	
+# \f	    Form Feed	
+# \ooo	Octal value	
+# \xhh	Hex value
+
+
+
+a = 'It\'s alright.'
+print(a)
+b = 'Let me insert \\ SARAN'
+print(b)
+c = 'I am the best\ndeveloper in \nthe world'
+print(c)
+d = 'I am the \rbest developer in the world'
+print(d)
+e = 'I am the best\tdeveloper in the world'
+print(e)
+f = 'I\b am the best developer in the world'
+print(f)
+g = 'I am the best developer in the world \f'
+print(g)
