3ab7c6c9f90b186adb00622f0377e97858b8e1ef
Galactic1239a <89837926+PythonFreelancer@users.noreply.github.com>
2022-10-18 06:54:54 +0530

    Python Tutorials

---
+++ Tutorial21.py
@@ -0,0 +1,58 @@
+# Python Lists
+
+## List
+
+list1 = ['Kawakaski','Pulsar','Ducati','Dodge Tommawok']
+print(list1)
+
+## List Items
+
+##Ordered
+##Changeable
+
+## Allow Duplicates
+
+list = ['Milk','Nuts','Chicken']
+print(list)
+
+## List Length
+
+list2 = ["apple", "banana", "cherry"]
+print(len(list2))
+
+## List Items - Data Types
+
+list1 = [12,6,2008,2,4,5,6]                       #int
+list2 = ['Saran','Abhinav','Shriram','Sugan']     #str
+list3 = [0.5,99.9,67.5,12.5,100.0]                #float
+list4 = [False,True,False,True,True,False]        #bool
+list5 = [12,'Saran',0.5,False]                    #Mixed
+print((list1))
+print((list2))
+print((list3))
+print((list4))
+print((list5))
+
+## type()
+
+list0 = [12,'Saran',0.5,False]
+print(type(list0))
+
+## The list() Constructor
+
+fruits = list(('Apple','Banana','Water melon','Dragon Fruit'))
+print(fruits)
+print(type(fruits))
+
+## Python Collections (Arrays)
+
+'''
+
+There are four collection data types in the Python programming language:
+
+List is a collection which is ordered and changeable. Allows duplicate members.
+Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+Dictionary is a collection which is ordered** and changeable. No duplicate members.
+
+'''
